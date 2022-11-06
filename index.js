5180899ee332a5ea74462579726dcfe36e190cb9
zonemeen <miqilin18@gmail.com>
2022-11-06 12:30:13 +0800

    修改vercel主文件

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path')
 
 const readTemplateFile = () => fs.readFileSync(path.join(__dirname, '163.svg'), 'utf-8')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     res.setHeader('content-type', 'image/svg+xml')
+    res.statusCode = 200
     res.send(readTemplateFile())
 }
