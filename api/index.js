1434eecb1e3f27e6eae987b0e11613541af29972
zonemeen <miqilin18@gmail.com>
2022-11-06 11:50:37 +0800

    修改index.js请求

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 const readTemplateFile = () => fs.readFileSync('../163.svg', 'utf-8')
 
-export default async (request, response) => {
-    response.setHeader('content-type', 'image/svg+xml')
-    response.send(readTemplateFile())
+export default (req, res) => {
+    res.setHeader('content-type', 'image/svg+xml')
+    res.send(readTemplateFile())
 }
